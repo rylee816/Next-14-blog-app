69352aa482dc046695a1f6f09c87858030c78305
Randy Riley <rylee777@gmail.com>
2024-09-11 07:48:57 -0400

    add mobile links

--- src/components/Navbar/links/Links.jsx
+++ src/components/Navbar/links/Links.jsx
@@ -1,49 +1,67 @@
+"use client";
 import NavLink from "./navLink/NavLink";
-import React from "react";
-import Styles from '../navbar.module.css'
+import React, { useState } from "react";
+import Styles from "../navbar.module.css";
+
+let routes = [
+  {
+    title: "Home",
+    path: "/",
+  },
+  {
+    title: "About",
+    path: "/about",
+  },
+  {
+    title: "Blog",
+    path: "/blog",
+  },
+  {
+    title: "Contact",
+    path: "/contact",
+  },
+];
 
 export default function Links() {
-
-  let routes = [
-    {
-      title: "Home",
-      path: "/",
-    },
-    {
-      title: "About",
-      path: "/about",
-    },
-    {
-      title: "Blog",
-      path: "/blog",
-    },
-    {
-      title: "Contact",
-      path: "/contact",
-    },
-
-
-  ];
+  const [isOpen, setIsOpen] = useState(false);
 
   // TEMPORARY
   const session = true;
   const isAdmin = false;
 
   return (
-    <div className={Styles.navLinks}>
-      {routes.map((route, idx) => 
-      (
-        <NavLink key={idx} route={route}/>
-      ))}
-      {session ? (
-        <>
-        {isAdmin && <NavLink route={{title: 'Admin', path: '/admin'}} />}
-        <button className={Styles.signoutBtn}>Sign Out</button>
-        </>
-      ) : (
-        <NavLink path={{title: 'Login', path: '/login'}}/>
-      )
-    }
+    <div>
+      <div className={Styles.navLinks}>
+        {routes.map((route, idx) => (
+          <NavLink key={idx} route={route} />
+        ))}
+        {session ? (
+          <>
+            {isAdmin && <NavLink route={{ title: "Admin", path: "/admin" }} />}
+            <button className={Styles.signoutBtn}>Sign Out</button>
+          </>
+        ) : (
+          <NavLink path={{ title: "Login", path: "/login" }} />
+        )}
+      </div>
+      <button onClick={() => setIsOpen(!isOpen)}>Menu</button>
+      {isOpen && (
+        <div className={isOpen && Styles.mobileLinks}>
+          {routes.map((route, idx) => (
+            <NavLink key={idx} route={route} />
+          ))}
+          {session ? (
+            <>
+              {isAdmin && (
+                <NavLink route={{ title: "Admin", path: "/admin" }} />
+              )}
+              <button className={Styles.signoutBtn}>Sign Out</button>
+            </>
+          ) : (
+            <NavLink path={{ title: "Login", path: "/login" }} />
+          )}
+        </div>
+      )}
     </div>
   );
 }
