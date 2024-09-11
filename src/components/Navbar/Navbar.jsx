c9b8f1db678e8a5b91ba25fa862da8c37052db47
Randy Riley <rylee777@gmail.com>
2024-09-11 19:36:02 -0400

    add woozle logo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,12 +2,15 @@ import Link from "next/link";
 import React from "react";
 import Links from "./links/Links";
 import Styles from './navbar.module.css'
+import Image from "next/image";
 
 export default function Navbar() {
   return (
     <div className={Styles.Navbar}>
-      <div className="brand">
-        <Link href="/">LOGO</Link>
+      <div className={Styles.brand}>
+        <Link href="/">
+          <Image className={Styles.image} src='/woozleimage.png' fill alt="woozle" />
+        </Link>
       </div>
       <div className={Styles.navLinks}>
         <Links />
