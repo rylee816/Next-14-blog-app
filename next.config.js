e5f15dd968550df9ee1ab688ebb9d3229a733add
Randy Riley <rylee777@gmail.com>
2024-09-23 21:00:10 -0400

    add github photos to config, add stylesheet for admin posts

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
     images: {
         domains: ['images.pexels.com', 'picsum.photos', 'via.placeholder.com'],
+        remotePatterns: [
+            {
+              protocol: "https",
+              hostname: "**",
+            },
+          ],
     },
 
 }
--- src/components/adminPostForm/AdminPostForm.jsx
+++ src/components/adminPostForm/AdminPostForm.jsx
@@ -2,25 +2,28 @@
 import { createPost } from '@/lib/actions'
 import React from 'react'
 import { useFormState } from 'react-dom'
+import Styles from './adminPostForm.module.css'
 
 export default function AdminPostForm({ userId }) {
-    console.log(userId)
     const [_, formAction] = useFormState(createPost, undefined)
 
     return (
-        <div className="adminPost">
-            <form action={formAction}>
-                <input type="text" name="title" placeholder="Title" />
-                <input type="text" name="slug" placeholder="Slug" />
-                <input type="text" name="image" placeholder="Image" />
-                <input type="hidden" name="userId" value={userId} />
-                <input
-                    type="textarea"
+        <div className={Styles.adminPostForm}>
+            <form className={Styles.adminPost} action={formAction}>
+                <input className={Styles.input} type="text" name="title" placeholder="Title" />
+                <input className={Styles.input} type="text" name="slug" placeholder="Slug" />
+                <input className={Styles.input} type="text" name="image" placeholder="Image" />
+                <input className={Styles.input} type="hidden" name="userId" value={userId} />
+                <textarea className={Styles.input}
+                    type="text"
                     name="body"
                     placeholder="Body"
                     rows={10}
                 />
-                <button>Create Post</button>
+                <div className={Styles.submitBtn}>
+                <button className={Styles.btn}>Create Post</button>
+
+                </div>
             </form>
         </div>
     )
