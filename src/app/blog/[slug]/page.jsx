b479fd84eaeea5849bd81128a9e3f47c545b2fcb
Randy Riley <rylee777@gmail.com>
2024-09-27 10:26:00 -0400

    fix styling on viewPost

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,4 +1,4 @@
-import React, {Suspense} from 'react'
+import React, { Suspense } from 'react'
 import Styles from './viewPost.module.css'
 import Image from 'next/image'
 import PostUser from '@/components/PostUser/PostUser'
@@ -6,46 +6,45 @@ import { getPost } from '@/lib/data'
 import { auth } from '@/lib/auth'
 import { redirect } from 'next/navigation'
 
+export const generateMetadata = async ({ params }) => {
+    const session = await auth()
+    //   if(!session){
+    //     redirect('/')
+    //   }
+    const { slug } = params
+    const post = await getPost(slug)
 
-export const generateMetadata = async ({params}) => {
-  const session = await auth()
-//   if(!session){
-//     redirect('/')
-//   }
-  const {slug} = params;
-  const post = await getPost(slug)
-
-  return {
-    title: post.title,
-    description: post.body
-  }
+    return {
+        title: post.title,
+        description: post.body,
+    }
 }
-
 
 export default async function ViewPost({ params }) {
     const { slug } = params
     const post = await getPost(slug)
-    const formattedBody = post.body.split('\n').map((line, index) => (
-        <p key={index}>{line}</p>
-      ));
+    const formattedBody = post.body
+        .split('\n')
+        .map((line, index) => <p key={index}>{line}</p>)
+        
     return (
         <div className={`wrapper ${Styles.viewPost}`}>
             <div className={Styles.imgContainer}>
                 <Image
-                    src={post?.image || 'https://images.pexels.com/photos/3937174/pexels-photo-3937174.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'}
+                    src={
+                        post?.image ||
+                        'https://images.pexels.com/photos/3937174/pexels-photo-3937174.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+                    }
                     alt=""
                     fill
                 />
             </div>
             <div className={Styles.textContainer}>
                 <h1 className={Styles.title}>{post?.title}</h1>
-                <Suspense fallback={"...Loading"}>
+                <Suspense fallback={'...Loading'}>
                     <PostUser userId={post?.userId} />
                 </Suspense>
-    
-                {/* <p className={Styles.text}>{post?.body}</p>
-                 */}
-                 {formattedBody}
+                <div className={Styles.text}>{formattedBody}</div>
             </div>
         </div>
     )
