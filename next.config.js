9e50519a779925cdd7aaca633aad0a34eb87d01c
Randy Riley <rylee777@gmail.com>
2024-09-29 07:59:36 -0400

    change config file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  runtime: 'nodejs',
     images: {
         domains: ['images.pexels.com', 'picsum.photos', 'via.placeholder.com'],
         remotePatterns: [
